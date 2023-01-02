c38a025a492191e6b8de27d14ce61dda61545fde
seongjin <seongjin2427@gmail.com>
2023-01-02 23:38:06 +0900

    2023-01-02 문제풀이

--- index.js
+++ index.js
@@ -1,22 +1,120 @@
-const read = `1`;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-const input = +read.toString().trim();
+class BST {
+  constructor(head) {
+    this.head = head;
+  }
+  insert(value) {
+    let cNode = this.head;
+    while (true) {
+      if (value < cNode.value) {
+        if (cNode.left !== null) {
+          cNode = cNode.left;
+        } else {
+          cNode.left = new Node(value);
+          break;
+        }
+      } else {
+        if (cNode.right !== null) {
+          cNode = cNode.right;
+        } else {
+          cNode.right = new Node(value);
+          break;
+        }
+      }
+    }
+  }
+  search(value) {
+    let cNode = this.head;
+    while (cNode) {
+      if (cNode.value === value) {
+        return true;
+      } else if (value < cNode.value) {
+        cNode = cNode.left;
+      } else {
+        cNode = cNode.right;
+      }
+    }
+    return false;
+  }
+  remove(value) {
+    let searched = false;
+    let cNode = this.head;
+    let parent = this.head;
+    while (cNode) {
+      if (cNode.value === value) {
+        searched = true;
+        break;
+      } else if (value < cNode.value) {
+        parent = cNode;
+        cNode = cNode.left;
+      } else {
+        parent = cNode;
+        cNode = cNode.right;
+      }
+    }
 
-let money = 1000 - input;
-let count = 0;
+    if (!searched) {
+      return false;
+    }
 
-const calculate = (m) => {
-  let cash = m;
-  const units = [500, 100, 50, 10, 5, 1];
+    // cNode가 삭제할 Node, parent는 삭제할 Node의 Parent Node인 상태
+    if (cNode.left === null && cNode.right === null) {
+      if (value < parent.value) {
+        parent.left = null;
+      } else {
+        parent.right = null;
+      }
+      cNode = null;
+    }
 
-  const cal = (m, u) => {
-    count += Math.floor(m / u);
-    cash = m % u;
-  };
+    if (cNode.left !== null && cNode.right === null) {
+      if (value < parent.value) {
+        parent.left = cNode.left;
+      } else {
+        parent.right = cNode.left;
+      }
+    } else if (cNode.left === null && cNode.right !== null) {
+      if (value < parent.value) {
+        parent.left = cNode.right;
+      } else {
+        parent.right = cNode.right;
+      }
+    }
 
-  units.forEach((u) => cal(cash, u));
-};
+    let changeNodeParent;
+    let changeNode;
+    // case 3
+    if (cNode.left !== null && cNode.right !== null) {
+      // case 3-1
+      if (value < parent.value) {
+        changeNode = cNode.right;
+        changeNodeParent = cNode.right;
+        while (cNode.left) {
+          changeNodeParent = changeNode;
+          changeNode = changeNode.left;
+        }
+        if (changeNode.right !== null) {
+          changeNodeParent.left = changeNode.right;
+        } else {
+          changeNodeParent.left = null;
+        }
+      } else {
+      }
+    }
+  }
+}
 
-calculate(money);
-
-console.log(count);
+const node = new Node(50);
+const bst = new BST(node);
+bst.insert(20);
+bst.insert(100);
+bst.insert(30);
+console.log(bst.search(100));
+console.log(bst);
