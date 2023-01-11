9343d3bff06fc17e96912919d5e5c719f0362e44
seongjin <seongjin2427@gmail.com>
2023-01-11 17:19:26 +0900

    2022-01-11 퀵정렬,병합정렬 구현

---
+++ tools/quick-sort.js
@@ -0,0 +1,15 @@
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (pivot > arr[i]) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
